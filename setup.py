aef23913079a658e364328fbb3d32e81f725fd61
dwrickey <d.wrickey@gmail.com>
2020-08-02 14:40:22 -0500

    wrote README and updated some packaging-related files

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 
 setup(
-    name='git-scripts-Grimmslaw',
+    name='git-scripts',
     version='1.0.0',
     author='Wes Rickey',
     author_email='d.wrickey@gmail.com',
